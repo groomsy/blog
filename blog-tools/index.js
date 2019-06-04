e5bbaa356e78e1cc8d7cc40a796cd77f11f3199a
Todd Grooms <t@toddgrooms.com>
2019-06-04 08:38:29 -0700

    Setup micro page

--- blog-tools/index.js
+++ blog-tools/index.js
@@ -1,3 +1,18 @@
+function generateDateTimeMetadata(date, callback) {
+  var dateString = date.getFullYear() + "-" + String("0" + (date.getMonth() + 1)).slice(-2) + "-" + String("0" + date.getDate()).slice(-2)
+  
+  var hours = String("0" + date.getHours()).slice(-2)
+  var minutes = String("0" + date.getMinutes()).slice(-2)
+  var seconds = String("0" + date.getSeconds()).slice(-2)
+  
+  var timezoneOffsetSign = date.getTimezoneOffset() < 0 ? "+" : "-"
+  
+  var timeString = hours + ":" + minutes + ":" + seconds + " " + timezoneOffsetSign + String("0" + (date.getTimezoneOffset() / 60)).slice(-2) + "00"
+  var timeStringSlug = hours + minutes + seconds
+  
+  callback(dateString, timeString, timeStringSlug)
+}
+
 var args = process.argv.slice(2)
 if (args.length < 2 || args.length > 3) {
   console.error("Must have a command and an option!")
@@ -20,12 +35,9 @@ if (option == "post") {
   console.log("Creating post…")
   
   var date = new Date()
-  var dateString = date.getFullYear() + "-" + String("0" + (date.getMonth() + 1)).slice(-2) + "-" + String("0" + date.getDate()).slice(-2)
-  var timezoneOffsetSign = date.getTimezoneOffset() < 0 ? "+" : "-"
-  var timeString = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " " + timezoneOffsetSign + String("0" + (date.getTimezoneOffset() / 60)).slice(-2) + "00"
-  
-  var frontMatter =
-  `---
+  generateDateTimeMetadata(date, function(dateString, timeString, timeStringSlug) {
+    var frontMatter =
+`---
 layout:     post
 title:      "${title}"
 subtitle:   
@@ -35,16 +47,33 @@ date:       ${dateString} ${timeString}
 tags:       personal/development
 link:       
 ---`
-  console.log("Front Matter:")
-  console.log(frontMatter)
-  if (title.length > 0) {
-    var slug = require('slug')
-    var postSlug = dateString + "-" + slug(title).toLowerCase()
-    console.log("Slug: " + postSlug)
-  }
+    console.log("Front Matter:")
+    console.log(frontMatter)
+    if (title.length > 0) {
+      var slug = require('slug')
+      var postSlug = dateString + "-" + slug(title).toLowerCase()
+      console.log("Slug: " + postSlug)
+    }
+  })
 }
 else if (option == "toot") {
   console.log("Creating toot…")
+  
+  var date = new Date()
+  generateDateTimeMetadata(date, function(dateString, timeString, timeStringSlug) {
+    var frontMatter =
+`---
+layout: post
+title: null
+date: ${dateString} ${timeString}
+categories: [micro]
+hidden: true
+---`
+    console.log("Front Matter:")
+    console.log(frontMatter)
+    var postSlug = dateString + "-" + timeStringSlug
+    console.log("Slug: " + postSlug)
+  })
 }
 else {
   console.error("Unknown option " + option)
